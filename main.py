ee481dca13d440e6a6ce13d050d7c59a1ff04dcc
mohit <mohitrathor8@gmail.com>
2021-12-27 14:00:37 +0530

    adding menu in main program

--- main.py
+++ main.py
@@ -1,50 +1,13 @@
-import requests
-import datetime as date
+from api import Pixela
 
-SITE = "https://pixe.la/v1/users"
-USERNAME = "mohitrathor"
-TOKEN = "sadakhk2h3nj8snsa"
-
-# create user id
-request_body = {
-    "token": TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes"
-}
-
-# create new graph
-graph_site = f"{SITE}/{USERNAME}/graphs"
-graph_id = "graph1"
-headers = {
-    "X-USER-TOKEN": TOKEN
-}
-graph_request = {
-    "id": graph_id,
-    "name": "Coding Graph",
-    "unit": "commit",
-    "type": "int",
-    "color": "ajisai"
-}
-
-
-# post the progress
-post_site = f"{SITE}/{USERNAME}/graphs/{graph_id}"
-
-today = date.datetime.now()
-date_format = today.strftime("%Y%m%d")
-
-post_body = {
-    "date": date_format,
-    "quantity": input("Number of commits: ")
-}
-
-# update the progress
-update_site = f"{SITE}/{USERNAME}/graphs/{graph_id}/{date_format}"
-
-update_body = {
-    "quantity": input(f"Update value for {date_format}: ")
-}
-
-response = requests.put(url=update_site, json=update_body, headers=headers)
-print(response.status_code)
+while True:
+    print("\n1) Create User\n2) Create Graph\n3) Post progress\n4) Update Progress\n5) Delete Progress\n6) Exit\n")
+    try:
+        choice = input("Your Choice: ")
+    except:
+        break
+    
+    pixela_ob = Pixela()
+    if choice == 1:
+        username = input("Username: ")
+        print(pixela_ob.create_user(username))
